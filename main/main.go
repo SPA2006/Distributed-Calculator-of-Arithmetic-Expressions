4f026c75a4926d999638d8a30ede01f0f709cc9a
SPA2006 <67869687+SPA2006@users.noreply.github.com>
2024-03-02 23:33:21 +0300

    bug fixes

--- main/main.go
+++ main/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"net/http"
 	"strconv"
-	"strings"
 
 	agent "calc/agent"
 	model "calc/internal/base_model"
@@ -16,105 +14,91 @@ func main() {
 	Duration := make(map[model.OperationClass]int)
 	model.SetDuration(Duration)
 
-	var durrr []int
-	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Building REST APIs in GO")
+	//  создаём новый мультиплексор
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "--- SLOWED CALCULATOR ---")
+	})
+	// блок кода для обработки обращения на http-сервер через
+	// curl -X GET или POST http://localhost:8080/
+	mux.HandleFunc("GET /duration", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "addition:       %d\n", Duration["addition"])
+		fmt.Fprintf(w, "subtraction:    %d\n", Duration["subtraction"])
+		fmt.Fprintf(w, "multiplication: %d\n", Duration["multiplication"])
+		fmt.Fprintf(w, "division:       %d\n", Duration["division"])
+	})
 
-	for {
-		fmt.Println("Будете вводить время выполнения операций?")
-		fmt.Println("--- по умолчанию время выполнения каждой операции -- 50 сек ---")
-		fmt.Println()
-		fmt.Println("Введите true, если да, false -- если нет")
-		str_ans, _ := reader.ReadString('\n')
-		str_ans = strings.TrimSpace(str_ans)
-		ans, err := strconv.ParseBool(str_ans)
+	mux.HandleFunc("POST /duration/addition/{duration}", func(w http.ResponseWriter, r *http.Request) {
+		val, _ := strconv.Atoi(r.PathValue("duration"))
+		readDuration(model.Addition, val, Duration)
+	})
 
-		if err != nil {
-			for {
-				fmt.Println("Вы ввели неправильно значение")
-				fmt.Println("Введите true, если да, false -- если нет")
-				str_ans, _ = reader.ReadString('\n')
-				ans, err = strconv.ParseBool(str_ans)
+	mux.HandleFunc("POST /duration/substraction/{duration}", func(w http.ResponseWriter, r *http.Request) {
+		val, _ := strconv.Atoi(r.PathValue("duration"))
+		readDuration(model.Subtraction, val, Duration)
+	})
 
-				if err == nil {
-					break
-				}
-			}
+	mux.HandleFunc("POST /duration/multiplication/{duration}", func(w http.ResponseWriter, r *http.Request) {
+		val, _ := strconv.Atoi(r.PathValue("duration"))
+		readDuration(model.Multiplication, val, Duration)
+	})
+
+	mux.HandleFunc("POST /duration/division/{duration}", func(w http.ResponseWriter, r *http.Request) {
+		val, _ := strconv.Atoi(r.PathValue("duration"))
+		readDuration(model.Division, val, Duration)
+	})
+
+	// реализация запроса на сервер
+	// получаем от сервера результат выражения, введённого пользоавтелем
+
+	mux.HandleFunc("GET /{expression}", func(w http.ResponseWriter, r *http.Request) {
+		expression := r.PathValue("expression")
+		result, err := agent.Reading_notation(test.Testing_cases[expression], Duration)
+		err_check(w, result, err)
+	})
+
+	// получаем от сервера результат тестового выражения id (название), которого ввёл пользователь
+
+	mux.HandleFunc("GET /test/{test_case}", func(w http.ResponseWriter, r *http.Request) {
+		case_name := r.PathValue("test_case")
+
+		_, ok := test.Testing_cases[case_name]
+
+		if ok {
+			result, err := agent.Reading_notation(test.Testing_cases[case_name], Duration)
+			err_check(w, result, err)
 		}
+	})
 
-		if ans {
-			fmt.Println("Введите время выполнения операций:")
-			durrr = readDuration("Сложение", durrr)
-			durrr = readDuration("Вычитание", durrr)
-			durrr = readDuration("Умножение", durrr)
-			durrr = readDuration("Деление", durrr)
-			model.ResetDuration(durrr, Duration)
-		}
-
-		fmt.Println("1. Проверить тестовые выражения")
-		fmt.Println("2. Ввести собственное выражение")
-		fmt.Println("3. Выход")
-		str_ans, _ = reader.ReadString('\n')
-		str_ans = strings.TrimSpace(str_ans)
-
-		switch str_ans {
-		case "1":
-			fmt.Println("Введите название теста -- слово перед двоеточием:")
-			fmt.Println("standard:   1+1")
-			fmt.Println("suboptimal: 01+1")
-			fmt.Println("letter:     1+a")
-			fmt.Println("symbol:     +-/*")
-			fmt.Println("number:     12345")
-			fmt.Println("bracket:    (1+2")
-
-			type_ans, _ := reader.ReadString('\n')
-			type_ans = strings.TrimSpace(type_ans)
-
-			result, err := agent.Reading_notation(test.Testing_cases[type_ans], Duration)
-			err_check(result, err)
-
-		case "2":
-			fmt.Println("Введите собственное выражение:")
-			type_ans, _ := reader.ReadString('\n')
-			type_ans = strings.TrimSpace(type_ans)
-
-			result, err := agent.Reading_notation(type_ans, Duration)
-			err_check(result, err)
-
-		case "3":
-			break
-		default:
-			fmt.Println("Введите правильный ответ")
-		}
+	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
+		fmt.Println(err.Error())
 	}
 }
 
-func readDuration(operation string, durrr []int) []int {
-	var val int
-	fmt.Printf("%s: ", operation)
-	fmt.Fscan(os.Stdin, &val)
-	durrr = append(durrr, val)
-	return durrr
+func readDuration(operation model.OperationClass, val int, Duration map[model.OperationClass]int) {
+	Duration[operation] = val
 }
 
 // проверяет наличие ошибки
-func err_check(result int, err error) {
+func err_check(w http.ResponseWriter, result int, err error) {
 	if err != nil {
 		switch err.Error() {
 		case "zero":
-			fmt.Println("В выражении есть незначащий ноль")
+			fmt.Fprintf(w, "В выражении есть незначащий ноль")
 		case "letter":
-			fmt.Println("В выражении есть буква(-ы)")
+			fmt.Fprintf(w, "В выражении есть буква(-ы)")
 		case "symbol":
-			fmt.Println("В выражении есть лишний символ")
+			fmt.Fprintf(w, "В выражении есть лишний символ")
 		case "number":
-			fmt.Println("В выражении находятся только числа")
+			fmt.Fprintf(w, "В выражении находятся только числа")
 		case "bracket":
-			fmt.Println("В выражении есть лишняя скобка")
+			fmt.Fprintf(w, "В выражении есть лишняя скобка")
 		default:
-			fmt.Println("Неизвестная ошибка:", err)
+			fmt.Fprintf(w, "Неизвестная ошибка: %s", err.Error())
 		}
 	} else {
-		fmt.Println("Результат выражения")
+		fmt.Fprintf(w, "")
 	}
-	fmt.Println("answer: ", result)
+	fmt.Fprintf(w, "Результат выражения: %d", result)
 }
